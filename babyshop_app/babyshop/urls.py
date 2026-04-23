48f6f7c0baf1aacc73eabb708f51c6fd681c824a
Philipp Klinger <klinger.breuber@web.de>
2026-04-23 16:42:11 +0200

    fix: persist and serve uploaded media files

--- babyshop_app/babyshop/urls.py
+++ babyshop_app/babyshop/urls.py
@@ -13,14 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+import os
+
 from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('products.urls')),
-    path('users/',include('users.urls')),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('', include('products.urls')),
+    path('users/', include('users.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if os.environ.get("DJANGO_SERVE_MEDIA", "False").lower() in ("1", "true", "yes", "on"):
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
